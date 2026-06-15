5a6a65af4b3603d43b43d3ca7738ef7077917c90
gitter <git@totalgood.com>
2026-06-14 18:04:58 -0700

    bug

--- scripts/monitor_train_progress.sh
+++ scripts/monitor_train_progress.sh
@@ -1,3 +1,3 @@
 tail  -q -n1 results/hypertune/trial_???/train_progress.jsonl > train_progress_tail1.jsonl
 ls -1 results/hypertune/trial_00?/train_progress.jsonl > train_progress_tail1_paths.txt
-python -c 'import pandas as pd; print(pd.read_json("train_progress_tail1.jsonlines", lines=True));' 
+python -c 'import pandas as pd; print(pd.read_json(open("train_progress_tail1.jsonl"), lines=True));' 
