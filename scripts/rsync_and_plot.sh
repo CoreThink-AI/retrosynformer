baf7a8316df7d7766a6a921e98754231b67d32e2
hobs <git@totalgood.com>
2026-06-17 12:40:57 -0700

    review-hyperparam-table-tbd

--- scripts/rsync_and_plot.sh
+++ scripts/rsync_and_plot.sh
@@ -1,1 +1,16 @@
-export STUDY_NAME=standard-v2-dropout-details && rsync -av --include='*/' --include='study.db' --include='*.yaml' --include='pred_routes_train_progress.json' --include='train_progress.jsonl' --include '*config*' --include '*.log' --exclude='*' taco:code/corethink/retrosynformer/results/ results/ && rs-plot-learning-curves --study standard-v2-dropout-details --yscale linear
+#!/usr/bin/env bash
+export STUDY_NAME=standard-v2-dropout-details \
+  && rsync -av \
+    --include='*/' \
+      --include='study.db' \
+      --include='*.yaml' \
+      --include='pred_routes_train_progress.json' \
+      --include='train_progress.jsonl' \
+      --include '*config*' \
+      --include '*.log' \
+    --exclude='*' \
+    taco:code/corethink/retrosynformer/results/ \
+    results/  \
+  && rs-plot-learning-curves \
+    --study standard-v2-dropout-details \
+    --yscale linear
