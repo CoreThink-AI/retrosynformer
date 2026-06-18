eeefa793ca4e60b33e51f110e8923b23aaffb79b
hobs <git@totalgood.com>
2026-06-17 21:05:47 -0700

    rsync command accidentally overwrote local changes

--- scripts/rsync_and_plot.sh
+++ scripts/rsync_and_plot.sh
@@ -4,18 +4,18 @@ export STUDY_NAME=standard-v2-dropout-details
 if [[ -n $1 ]] ; then
    STUDY_NAME=$1
 fi
-
 rsync -av \
-    --include='*/' \
-      --include='study.db' \
-      --include='*.yaml' \
-      --include='pred_routes_train_progress.json' \
-      --include='train_progress.jsonl' \
-      --include '*config*' \
-      --include '*.log' \
-    --exclude='*' \
-    taco:code/corethink/retrosynformer/results/ \
-    results/  \
-&& rs-plot-learning-curves \
+  --include='*/' \
+    --include='study.db' \
+    --include='pred_routes_train_progress.json' \
+    --include='train_progress.jsonl' \
+    --include '*.log' \
+    --include='model.config.yaml' \
+    --include='run.jsonl' \
+  --exclude='*' \
+  taco:code/corethink/retrosynformer/results/ \
+  results/
+
+rs-plot-learning-curves \
     --study $STUDY_NAME \
     --yscale linear
