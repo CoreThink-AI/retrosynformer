ce19924991608f7f4e9eed235620dd210e0ed24d
hobs <git@totalgood.com>
2026-06-20 15:30:28 -0700

    fit test

--- src/retrosynformer/fit.py
+++ src/retrosynformer/fit.py
@@ -2,27 +2,31 @@ import math
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import Ridge, Lasso
+from sklearn.preprocessing import PolynomialFeatures
+from matplotlib import pyplot as plt
+
 
 df = pd.DataFrame(list(range(1, 151)), columns=['epoch'])
-df['y'] = df['epoch'].apply(np.log)
+bias, sigma = .5, .5
+df['y'] = df['epoch'].apply(np.log) + sigma * np.random.randn(len(df)) + bias
 df = df.set_index('epoch')
 
-from sklearn.preprocessing import PolynomialFeatures
-p = PolynomialFeatures(df['epoch'])
-p = PolynomialFeatures(df['epoch'], degree=2)
-p = PolynomialFeatures?
-p = PolynomialFeatures(degree=2, df['epoch'])
 p = PolynomialFeatures(degree=2)
-p.fit(df['epoch'])
-p?
-p.fit_transform(df['epoch'])
-p.fit_transform(df[['epoch']])
-p.fit_transform?
-pd.DataFrame(p.fit_transform(df[['epoch']]))
-pd.concat(df, pd.DataFrame(p.fit_transform(df[[list(df.columns)[0]]])))
-pd.concat([df, pd.DataFrame(p.fit_transform(df[[list(df.columns)[0]]]))])
-pd.concat([df, pd.DataFrame(p.fit_transform(df[[list(df.columns)[0]]]))], axis=1)
-df = pd.concat([df, pd.DataFrame(p.fit_transform(df[[list(df.columns)[0]]]))], axis=1)
-df = pd.concat([df, pd.DataFrame(p.fit_transform(df[[list(df.columns)[0]]]), columns=[f'x_{i}' for i in range(p.degree+int(p.include_bias))])], axis=1)
-df
-hist -f src/retrosynformer/fit.ipy
+df = pd.concat([
+    df, 
+    pd.DataFrame(
+        p.fit_transform(
+            df[[list(df.columns)[0]]]),
+        columns=[f'x_{i}' for i in range(p.degree+int(p.include_bias))],
+        index=df.index)
+    ], axis=1)
+X = df[[c for c in df.columns if c.startswith('x_')]]
+Y = df[[c for c in df.columns if c.startswith('y')]]
+
+m = Ridge()
+m.fit(X, Y)
+df['y_pred'] = m.predict(X)
+df[[c for c in df.columns if c.startswith('y')]].plot()
+plt.grid('on')
+plt.show()
+
